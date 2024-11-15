28c36ef0a0cc3a9dee8389a69700db3fc5840acb
MelchizedekMunene <melchizedekmunene@gmail.com>
2024-11-15 19:35:13 +0300

    Mapping data onto the category section

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Data from '@/Shared/Data';
+import React from 'react';
 
 function Category() {
   return (
-    <div>
-      
+    <div className='mt-40'>
+      <h2 className='font-bold text-3xl text-center mb-6'>Browse By Type</h2>
+
+
+      <div className='grid grid-cols-3 gsm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9'>
+        {Data.Category.map((category,index) => (
+            <div key={index} className=''>
+                <img src={category.icon} alt={category.name} />
+                <h2 className=''>{category.name}</h2>
+            </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
