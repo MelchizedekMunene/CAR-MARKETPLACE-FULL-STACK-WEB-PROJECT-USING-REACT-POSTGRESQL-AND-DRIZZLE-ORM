54c6098c02babdaf228ab7202791e1e0e41c8be7
MelchizedekMunene <melchizedekmunene@gmail.com>
2024-11-16 03:21:50 +0300

    Displaying Product Cards

---
+++ src/components/CarItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarItem(car) {
+  return (
+    <div>
+      <img src={car.image} alt="" />
+    </div>
+  )
+}
+
+export default CarItem
--- src/components/MostSearchedCars.jsx
+++ src/components/MostSearchedCars.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FakeData from '@/Shared/FakeData';
+import CarItem from './CarItem';
 
 function MostSearchedCars() {
-    console.log(FakeData.carList);
   return (
     <div>
+        <h2 className='font-bold text-3xl text-center my-16'>Most Searched Cars</h2>
 
+        {FakeData.carList.map((car, index) => (
+            <CarItem car={car} key={index} />
+        ))}
     </div>
   )
 }
